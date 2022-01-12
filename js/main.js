fb38ad17732198700bd18a462bfc041ca2f42ff6
VictoriaOkazaki <victoria.timein@gmail.com>
2022-01-12 13:24:43 +0400

    all screens adaptation added

--- js/main.js
+++ js/main.js
@@ -4,5 +4,14 @@ $(function () {
       '<button class="slick-arrow slick-prev"><img src="images/prev.svg" alt="previous arrow"></button>',
     nextArrow:
       '<button class="slick-arrow slick-next"><img src="images/next.svg" alt="next arrow"></button>',
+
+    responsive: [
+      {
+        breakpoint: 441,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
 });
